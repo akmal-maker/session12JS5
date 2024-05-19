662cbd938caf7ce8a1c945899c6e173fe7099989
akmal-maker <64779340+akmal-maker@users.noreply.github.com>
2024-05-20 01:12:24 +0300

    last update 1:12am 5/20/2024

---
+++ task JS/script.js
@@ -0,0 +1,48 @@
+
+// ======================================== 1 ===========================
+
+// function printNumbers(){
+//     for(let i = 1; i <=10; i++){
+//         console.log(i);
+//     }
+// }
+// printNumbers()
+// ======================================== 2 ===========================
+
+// function printArray(){
+//     for(let i = 0; i = array.length;i++){
+//         for(let j = 0; j=array[i].length;j++){
+//             console.log(array[i][j]);
+//         }
+//     }
+// }
+// ======================================== 3 ===========================
+// var arr = [13,23,12,45,22,48,66,100]
+//  function printEvenNum(){
+//     for(let i = 0; i<arr.length;i++){
+//         if(arr[i] % 2 ==0){
+//             console.log(arr[i]);
+//         }
+//     }
+//  }
+
+//  printEvenNum()
+
+// ======================================== 4 ===========================
+// var arr = [23,56,4,78,5,63,45,210,56]
+
+// function deleteElement() {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === 56) {
+//             arr.splice(i, 1); 
+//             i--; 
+//         }
+//     }
+//     return arr;
+// }
+
+// deleteElement(arr,56)
+// console.log(arr);
+
+// ======================================== 5 ===========================
+
